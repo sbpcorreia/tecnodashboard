0eaca236fcc5c709c6e9e8a83c01aa2b66707f36
Pedro Correia <pcorreia@solid-bridge.pt>
2025-12-17 16:35:37 +0000

    Add: configuração de sqlsrv 3

--- config/database.php
+++ config/database.php
@@ -109,7 +109,7 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'encrypt' => env('DB_ENCRYPT', 'no'),
+            //'encrypt' => env('DB_ENCRYPT', 'no'),
             'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
