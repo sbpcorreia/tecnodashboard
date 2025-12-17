4679338314696e90d909f799cacff4f1fb093e4b
Pedro Correia <pcorreia@solid-bridge.pt>
2025-12-17 23:07:39 +0000

    Fix: Problema ao gravar alteração do responsável 11

--- config/database.php
+++ config/database.php
@@ -109,11 +109,8 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'encrypt' => 'no',
-            'trust_server_certificate' => true,
-            'options' => [
-                'TrustServerCertificate' => true,
-            ],
+            'encrypt' => env('DB_ENCRYPT', 'yes'),
+            'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', false),
         ],
 
     ],
